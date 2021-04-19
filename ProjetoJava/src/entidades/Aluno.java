7fe4c992018beace7cd468b36fc86895268a846a
Ramiro <Ramiro@DESKTOP-JTQ74AI>
2021-04-19 11:54:08 -0300

    Projeto Basico de notas de alunos

---
+++ ProjetoJava/src/entidades/Aluno.java
@@ -0,0 +1,186 @@
+package entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aluno {
+
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String rg;
+	private String cpf;
+	private String mae;
+	private String pai;
+	private String matricula;
+	private String serie;
+	private String escola;
+
+	private List<Disciplina> disciplinas = new ArrayList<>();
+
+	public Aluno() {
+	}
+
+	public Aluno(String nome, int idade, String dataNascimento, String rg, String cpf, String mae, String pai,
+			String matricula, String serie, String escola) {
+		this.nome = nome;
+		this.idade = idade;
+		this.dataNascimento = dataNascimento;
+		this.rg = rg;
+		this.cpf = cpf;
+		this.mae = mae;
+		this.pai = pai;
+		this.matricula = matricula;
+		this.serie = serie;
+		this.escola = escola;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getMae() {
+		return mae;
+	}
+
+	public void setMae(String mae) {
+		this.mae = mae;
+	}
+
+	public String getPai() {
+		return pai;
+	}
+
+	public void setPai(String pai) {
+		this.pai = pai;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getSerie() {
+		return serie;
+	}
+
+	public void setSerie(String serie) {
+		this.serie = serie;
+	}
+
+	public String getEscola() {
+		return escola;
+	}
+
+	public void setEscola(String escola) {
+		this.escola = escola;
+	}
+
+	public List<Disciplina> getListDisciplinas() {
+		return disciplinas;
+	}
+
+	public double mediaNotas() {
+		double soma = 0.0;
+		for (Disciplina x : disciplinas) {
+			soma += x.getNota();
+		}
+		return soma / disciplinas.size();
+	}
+
+	public boolean mediaGeral() {
+		double media = mediaNotas();
+		if (media >= 70) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public String resultado() {
+		double mediaAprovado = mediaNotas();
+		if (mediaAprovado >= 70) {
+			return "Aluno aprovado";
+		} else {
+			return "Aluno reprovado";
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", rg=" + rg
+				+ ", cpf=" + cpf + ", mae=" + mae + ", pai=" + pai + ", matricula=" + matricula + ", serie=" + serie
+				+ ", escola=" + escola + "";
+	}
+
+}
---
+++ ProjetoJava/src/exacuvavel/Programa.java
@@ -0,0 +1,47 @@
+package exacuvavel;
+
+import java.util.Locale;
+
+import javax.swing.JOptionPane;
+
+import entidades.Aluno;
+import entidades.Disciplina;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+
+		String nome = JOptionPane.showInputDialog("Nome do Aluno ?");
+		String idade = JOptionPane.showInputDialog("Idade do aluno ?");
+		String dataNascimento = JOptionPane.showInputDialog("Data de nascimento ?");
+		String rg = JOptionPane.showInputDialog("Registro geral do Aluno ?");
+		String cpf = JOptionPane.showInputDialog("CPF ?");
+		String mae = JOptionPane.showInputDialog("Nome da mãe ?");
+		String pai = JOptionPane.showInputDialog("Nome do Pai ?");
+		String dataMatricula = JOptionPane.showInputDialog("Data da matrícula do Aluno ?");
+		String matricula = JOptionPane.showInputDialog("Matrícula do Aluno ?");
+		String nomeEscola = JOptionPane.showInputDialog("Nome da escola ?");
+
+		Aluno aluno1 = new Aluno(nome, Integer.parseInt(idade), dataNascimento, rg, cpf, mae, pai, dataMatricula,
+				matricula, nomeEscola);
+
+	
+		for(int i = 0; i<=3;i++) {
+			String nomeDis = JOptionPane.showInputDialog("Nome da disciplina: ");
+			String notaDis = JOptionPane.showInputDialog("Nota do aluno na disciplina "+nomeDis+" ?");
+			
+			Disciplina disciplina = new Disciplina();
+			disciplina.setDisciplina(nomeDis);
+			disciplina.setNota(Double.parseDouble(notaDis));
+			
+			aluno1.getListDisciplinas().add(disciplina);
+		}
+		
+
+		System.out.println(aluno1);
+		System.out.println("Com media: " + aluno1.mediaNotas());
+		System.out.println("Resultado: " + aluno1.resultado());
+	}
+
+}
